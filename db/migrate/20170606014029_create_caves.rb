dd0c2f36660a99bebb5d3f4a709242e339036da7
walwoodr <walwoodr@gmail.com>
2017-06-05 21:56:30 -0500

    Figuring out the cafes versus caves question

--- db/migrate/20170606014029_create_caves.rb
+++
@@ -1,8 +0,0 @@
-class CreateCaves < ActiveRecord::Migration[5.0]
-  def change
-    create_table :caves do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170606014029) do
+ActiveRecord::Schema.define(version: 20170605224637) do
 
   create_table "cafes", force: :cascade do |t|
     t.string   "name"
@@ -26,11 +26,6 @@ ActiveRecord::Schema.define(version: 20170606014029) do
     t.integer  "teaQuality"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
-  end
-
-  create_table "caves", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
